e90c79db2025a2b8e8eb33176be5bd9247c0e0ea
unknown <ahmad.syarif4594@gmail.com>
2019-07-10 23:05:08 +0700

    konvert bahasa

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + (5/9 * (body.currently.temperature - 32)) + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' suhunya ' + (5/9 * (body.currently.temperature - 32)) + ' derajat celcius. ada peluang hujan sebesar ' + body.currently.precipProbability + '%')
         }
     })
 }
